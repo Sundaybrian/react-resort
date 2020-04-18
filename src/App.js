c4f82c3fedbc86ec9e4bfb92f0752c3e67f7b299
Brian Sunday <brians931@gmail.com>
2020-04-18 11:51:32 +0300

    setup dummy navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/rooms/" component={Rooms} />
