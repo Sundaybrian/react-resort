c6a118cd15aefba97691f894a9735600471d1bba
Brian Sunday <brians931@gmail.com>
2020-04-21 17:39:35 +0300

    add a hero and banner to single rooms

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,52 @@
 import React, { Component } from "react";
+import { RoomContext } from "../context/roomContext";
+import defaultBcg from "../images/room-1.jpeg";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { render } from "@testing-library/react";
 
 export default class SingleRoom extends Component {
+  static contextType = RoomContext;
+
+  state = {
+    slug: this.props.match.params.id,
+    defaultBcg: defaultBcg,
+  };
+
   render() {
-    return <div>hello from single room</div>;
+    let { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found</h3>
+          <Link to="/rooms" className="btn-primary">
+            Back to rooms
+          </Link>
+        </div>
+      );
+    }
+
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      pets,
+      breakfast,
+      extras,
+      images,
+    } = room;
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room `}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
