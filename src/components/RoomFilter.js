b71d5fb95a897a6a60d6dec9f0bc2f7dfdf83a35
Brian Sunday <brians931@gmail.com>
2020-04-22 15:44:49 +0300

    setup initial room type select firld

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
 import Title from "./Title";
+import { RoomContext } from "../context/roomContext";
 
 const RoomFilter = () => {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
   return (
-    <div>
+    <section className="filter-container">
       <Title title="search rooms" />
-    </div>
+      <form className="filter-form">
+        {/* select type */}
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {}
+          </select>
+        </div>
+        {/*  end select type*/}
+      </form>
+    </section>
   );
 };
 
